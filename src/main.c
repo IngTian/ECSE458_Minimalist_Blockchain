e49838e4087fa02296d2149c9d41ac5a23e5093b
Ing Tian <52404420+IngTian@users.noreply.github.com>
2022-10-28 00:06:32 -0400

    feat: completed block (#14)
    * add basic block
    
    * feat: change block
    
    * add basic block
    
    * test: write block test in main
    
    * fix: fix block bugs
    
    * style: formatted block
    
    Co-authored-by: JoeyChen-95 <707973013@qq.com>

--- src/main.c
+++ src/main.c
@@ -1,1 +1,19 @@
-int main() { return 0; }
+
+
+#include "model/block/block.h"
+int main() {
+    initialize_cryptography_system(true);
+    block* genesis_block=initialize_block_system();
+    block* b1=create_an_empty_block();
+    b1->header->version=114514;
+    finalize_block(b1);
+    append_prev_block(genesis_block,b1);
+
+    transaction* t;
+    t->version=999;
+
+    append_transaction_into_block(b1,t,0);
+
+    bool valid= verify_block_chain(b1);
+    return 0;
+}
---
+++ src/model/block/block.h
@@ -0,0 +1,41 @@
+#ifndef MINIMALIST_BLOCKCHAIN_SYSTEM_SRC_MODEL_BLOCK_H
+#define MINIMALIST_BLOCKCHAIN_SYSTEM_SRC_MODEL_BLOCK_H
+
+#include <stdbool.h>
+
+#include "../transaction/transaction.h"
+
+/*
+ * The following field is for defining blocks.
+ * For more details, please visit:
+ * https://developer.bitcoin.org/reference/block_chain.html
+ */
+
+typedef struct BlockHeader {
+    int version;                      // The block version number indicates which set of block validation rules to follow.
+    char prev_block_header_hash[65];  // A SHA256(SHA256()) hash in internal byte order of the previous block’s header.
+    char merkle_root_hash[65];        // A SHA256(SHA256()) hash in internal byte order.
+    unsigned int time;                // The block time is a Unix epoch time when the miner started hashing the header (according to the miner).
+    unsigned int nBits;               // An encoded version of the target threshold this block’s header hash must be less than or equal to.
+    unsigned int nonce;               // An arbitrary number miners change to modify the header hash for the PoW.
+} block_header;
+
+typedef struct Block {
+    block_header *header;    // The block header in the format described in the block header section.
+    transaction **txns;      // Every transaction in this block, one after another, in raw transaction format.
+    unsigned int txn_count;  // The total number of transactions in this block, including the coinbase transaction.
+
+} block;
+
+block *initialize_block_system();
+void destroy_block_system();
+block *create_an_empty_block(unsigned int);
+void destroy_block(block *);
+bool append_prev_block(block *prev_block, block *cur_block);
+bool finalize_block(block *);
+block *get_block_by_hash(char *);
+bool append_transaction_into_block(block *, transaction *, unsigned int input_idx);
+bool verify_block_chain(block *);
+char *get_genesis_block_hash();
+
+#endif
