cab1db6a5dfae5a937905673ed1173b77e22fc5b
IngTian <tzy20001018@gmail.com>
2022-11-24 18:20:31 -0500

    feat: defined transaction persistence interface

--- src/model/transaction/transaction_persistence.h
+++ src/model/transaction/transaction_persistence.h
@@ -1,4 +1,11 @@
 #ifndef MINIMALIST_BLOCKCHAIN_SYSTEM_SRC_MODEL_TRANSACTION_TRANSACTION_PERSISTENCE_H
 #define MINIMALIST_BLOCKCHAIN_SYSTEM_SRC_MODEL_TRANSACTION_TRANSACTION_PERSISTENCE_H
+#include <stdbool.h>
+
+#include "transaction.h"
+
+bool initialize_transaction_persistence();
+bool save_transaction(transaction *);
+transaction *get_transaction(char *txid);
 
 #endif
