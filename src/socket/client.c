b091feb43b68236e1d2665a041a55a08465b0edb
JoeyChen-95 <707973013@qq.com>
2023-02-14 14:18:08 -0500

    test

--- src/socket/client.c
+++ src/socket/client.c
@@ -2,37 +2,48 @@
 // programming
 #include <arpa/inet.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <stdlib.h>
+#include "../model/block/block.h"
+#include "../model/transaction/transaction.h"
 #define PORT 8080
+struct UsrData {
+    char usr_id[16];
+    char usr_pwd[16];
+    char usr_nickname[16];
+};
 
-int main(int argc, char const* argv[])
-{
-//    block_header_shortcut block_header={
-//        .prev_block_header_hash=NULL,
-//        .version=123,
-//        .nonce=0,
-//        .nBits=0,
-//        .merkle_root_hash=NULL,
-//        .time=114514
-//    };
-//
-//    //create a new block
-//    block_create_shortcut block_data={
-//        .header=&block_header,
-//        .transaction_list=NULL
-//    };
-//
-//    block* block1= (block *)malloc(sizeof(block));
-//    create_new_block_shortcut(&block_data,block1);
+struct Person {
+    int pid;
+    struct UsrData usrData;
+};
 
+
+int main(int argc, char const *argv[]) {
+    //    block_header_shortcut block_header={
+    //        .prev_block_header_hash=NULL,
+    //        .version=123,
+    //        .nonce=0,
+    //        .nBits=0,
+    //        .merkle_root_hash=NULL,
+    //        .time=114514
+    //    };
+    //
+    //    //create a new block
+    //    block_create_shortcut block_data={
+    //        .header=&block_header,
+    //        .transaction_list=NULL
+    //    };
+    //
+    //    block* block1= (block *)malloc(sizeof(block));
+    //    create_new_block_shortcut(&block_data,block1);
 
     int sock = 0, valread, client_fd;
     struct sockaddr_in serv_addr;
-    char* hello = "Hello, this is message from client!!!!";
-    char buffer[1024] = { 0 };
+    char *hello = "Hello, this is message from client!!!!";
+    char buffer[1024] = {0};
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("\n Socket creation error \n");
         return -1;
@@ -43,22 +54,36 @@ int main(int argc, char const* argv[])
 
     // Convert IPv4 and IPv6 addresses from text to binary
     // form
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)
-        <= 0) {
-        printf(
-            "\nInvalid address/ Address not supported \n");
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
         return -1;
     }
 
-    if ((client_fd
-         = connect(sock, (struct sockaddr*)&serv_addr,
-                   sizeof(serv_addr)))
-        < 0) {
+    if ((client_fd = connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr))) < 0) {
         printf("\nConnection Failed \n");
         return -1;
     }
-    send(sock, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
+
+
+
+//    transaction transaction1;
+//    transaction1.version=114514;
+//    transaction1.lock_time=2023;
+//    transaction1.tx_out_count=1;
+//    transaction1.tx_in_count=1;
+//    transaction_output output;
+//    output.value=99;
+//    memcpy(transaction1.tx_outs,(char *)&output, sizeof(output));
+    struct Person person;
+    struct UsrData sendUser;
+    person.pid=99;
+    memcpy(sendUser.usr_id, "100001", sizeof("100001"));
+    memcpy(sendUser.usr_pwd, "123456", sizeof("123456"));
+    memcpy(sendUser.usr_nickname, "Joe", sizeof("Joe"));
+    person.usrData=sendUser;
+
+    send(sock, (char *)&person, sizeof(person), 0);
+    printf("Client: person sent\n");
     valread = read(sock, buffer, 1024);
     printf("%s\n", buffer);
 
--- src/socket/server.c
+++ src/socket/server.c
@@ -6,16 +6,29 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include "../model/transaction/transaction.h"
+#include "../model/block/block.h"
 
 #define PORT 8080
-int main(int argc, char const* argv[])
-{
+
+struct UsrData {
+    char usr_id[16];
+    char usr_pwd[16];
+    char usr_nickname[16];
+};
+
+struct Person {
+    int pid;
+    struct UsrData usrData;
+};
+
+int main(int argc, char const *argv[]) {
     int server_fd, new_socket, valread;
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
-    char buffer[1024] = { 0 };
-    char* hello = "Hello, this is message from server!!!!";
+    char buffer[1024] = {0};
+    char *hello = "Hello, this is message from server!!!!";
 
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -24,9 +37,7 @@ int main(int argc, char const* argv[])
     }
 
     // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET,
-                   SO_REUSEADDR, &opt,
-                   sizeof(opt))) {
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
@@ -35,9 +46,7 @@ int main(int argc, char const* argv[])
     address.sin_port = htons(PORT);
 
     // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr*)&address,
-             sizeof(address))
-        < 0) {
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
@@ -45,15 +54,31 @@ int main(int argc, char const* argv[])
         perror("listen");
         exit(EXIT_FAILURE);
     }
-    if ((new_socket
-         = accept(server_fd, (struct sockaddr*)&address,
-                  (socklen_t*)&addrlen))
-        < 0) {
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
         perror("accept");
         exit(EXIT_FAILURE);
     }
-    valread = read(new_socket, buffer, 1024);
-    printf("%s\n",buffer);
+
+    transaction recvTx;
+    struct UsrData recvUser;
+    struct Person recvPerson;
+
+    recv(new_socket,buffer, sizeof(buffer),0);
+    memcpy(&recvPerson,buffer, sizeof(recvPerson));
+    recvUser=recvPerson.usrData;
+//    printf("Tx version: %d\n", recvTx.version);
+//    printf("Tx lock time: %d\n", recvTx.lock_time);
+//    printf("Tx out count: %d\n", recvTx.tx_out_count);
+//    printf("Tx in count: %d\n", recvTx.tx_in_count);
+//    printf("Tx out: %d\n", recvTx.tx_outs->value);
+    printf("Person pid: %d\n", recvPerson.pid);
+    printf("Person user data id: %s\n", recvUser.usr_id);
+
+
+
+
+
+
     send(new_socket, buffer, strlen(hello), 0);
     printf("Hello message sent\n");
 
