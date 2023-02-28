0c8ef2a8c28a99910471e3f0ee8da5125d7bfdc7
JoeyChen-95 <707973013@qq.com>
2023-02-27 21:49:22 -0500

    test: add client send block

--- src/socket/client.c
+++ src/socket/client.c
@@ -4,134 +4,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
 #include "../model/block/block.h"
 #include "../model/transaction/transaction.h"
-#include <stdlib.h>
-#include <strings.h>
-#include "utils/log_utils.h"
-
 #include "model/block//block.h"
 #include "model/transaction/transaction.h"
 #include "utils/constants.h"
 #include "utils/cryptography.h"
 #include "utils/log_utils.h"
 #include "utils/mysql_util.h"
+#include "utils/sys_utils.h"
 
 #define LOG_SCOPE "Client"
 #define PORT 8080
 
-char* combine_data_with_command(char *command, unsigned int command_length, const char* data, unsigned int data_length);
+char *combine_data_with_command(char *command, unsigned int command_length, const char *data, unsigned int data_length);
 
 int main(int argc, char const *argv[]) {
-    // Initialize the whole system.
-    mysql_config config = {.host_addr = "localhost",
-                           .username = "root",
-                           .password = "Admin123/",
-                           .db = "test",
-                           .port_number = 3306,
-                           .client_flag = CLIENT_MULTI_STATEMENTS};
-    initialize_mysql_system(config);
     initialize_cryptography_system(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY);
+
     transaction *previous_transaction = initialize_transaction_system();
     char *previous_output_private_key = get_genesis_transaction_private_key();
-    /** Create a sub-genesis transaction with 1 input from genesis transaction and 10001 outputs: 1*30960+10000*1 **/
-    char *previous_transaction_id = get_transaction_txid(previous_transaction);
 
-    int size = 5;
+    block *genesis_block = initialize_block_system();
+    block *block_under_test;
 
-    // Input from genesis tx
-    transaction_create_shortcut_input input_genesis = {
-        .previous_output_idx = 0, .previous_txid = previous_transaction_id, .private_key = previous_output_private_key};
-    // Create an output array of length 10001
-    transaction_create_shortcut_output *outputs_genesis = malloc((size + 1) * sizeof(transaction_create_shortcut_output));
-    unsigned char *outputs_genesis_private_keys[size + 1];
+    char *previous_block_header_hash = get_genesis_block_hash();
 
-    // Create 10000 outputs, each one has 1 value
-    for (int i = 0; i < size; i++) {
-        unsigned char *new_private_key_output_genesis = get_a_new_private_key();
-        secp256k1_pubkey *new_public_key_output_genesis = get_a_new_public_key((char *)new_private_key_output_genesis);
+    // Start testing.
 
-        transaction_create_shortcut_output output_genesis = {.value = 1, .public_key = ((char *)(new_public_key_output_genesis->data))};
+    for (int i = 0; i < 5; i++) {
+        char *previous_transaction_id = get_transaction_txid(previous_transaction);
+        transaction_create_shortcut_input input = {
+            .previous_output_idx = 0, .previous_txid = previous_transaction_id, .private_key = previous_output_private_key};
+        unsigned char *new_private_key = get_a_new_private_key();
+        secp256k1_pubkey *new_public_key = get_a_new_public_key((char *)new_private_key);
+        transaction_create_shortcut_output output = {.value = TOTAL_NUMBER_OF_COINS, .public_key = (char *)new_public_key->data};
 
-        outputs_genesis[i] = output_genesis;
-        outputs_genesis_private_keys[i] = new_private_key_output_genesis;
-    }
+        transaction_create_shortcut create_data = {.num_of_inputs = 1, .num_of_outputs = 1, .outputs = &output, .inputs = &input};
 
-    // Create 1 output, it has 30960 value
-    unsigned char *new_private_key_output_genesis_large = get_a_new_private_key();
-    secp256k1_pubkey *new_public_key_output_genesis_large = get_a_new_public_key((char *)new_private_key_output_genesis_large);
+        transaction *t = (transaction *)malloc(sizeof(transaction));
 
-    transaction_create_shortcut_output output_genesis_large = {.value = TOTAL_NUMBER_OF_COINS - size,
-                                                               .public_key = ((char *)(new_public_key_output_genesis_large->data))};
-
-    outputs_genesis[size] = output_genesis_large;
-    outputs_genesis_private_keys[size] = new_private_key_output_genesis_large;
-
-    transaction_create_shortcut create_data_genesis = {
-        .num_of_inputs = 1, .num_of_outputs = size + 1, .outputs = outputs_genesis, .inputs = &input_genesis};
-    transaction *t_sub_genesis = (transaction *)malloc(sizeof(transaction));
-
-    if (!create_new_transaction_shortcut(&create_data_genesis, t_sub_genesis)) {
-        general_log(LOG_SCOPE, LOG_ERROR, "Failed to create a transaction.");
-    } else {
-        general_log(LOG_SCOPE, LOG_INFO, "create a genesis sub transaction.");
-    }
-
-    verify_transaction(t_sub_genesis);
-
-    if (!finalize_transaction(t_sub_genesis)) {
-        general_log(LOG_SCOPE, LOG_ERROR, "Failed to finalize a transaction.");
-    }
-
-    previous_transaction = t_sub_genesis;
-
-    /** Create 10000*1 one to one transactions **/
-
-    transaction *small_txs = malloc(size * sizeof(transaction));
-    char *sub_genesis_transaction_id = get_transaction_txid(previous_transaction);
-    general_log(LOG_SCOPE, LOG_INFO, "Sub genesis hash: %s, its length: %d", sub_genesis_transaction_id, strlen(sub_genesis_transaction_id));
-    previous_transaction_id = sub_genesis_transaction_id;
-    unsigned char *previous_private_key;
-
-    for (int i = 0; i < size; i++) {
-        unsigned char *new_private_key_output_small = get_a_new_private_key();
-        secp256k1_pubkey *new_public_key_output_small = get_a_new_public_key((char *)new_private_key_output_small);
-
-        transaction_create_shortcut_input input_small = {
-            .previous_output_idx = i, .previous_txid = sub_genesis_transaction_id, .private_key = (char *)outputs_genesis_private_keys[i]};
-
-        transaction_create_shortcut_input input_large = {
-            .previous_output_idx = i == 0 ? size : 0,
-            .previous_txid = previous_transaction_id,
-            .private_key = i == 0 ? (char *)outputs_genesis_private_keys[size] : (char *)previous_private_key};
-
-        transaction_create_shortcut_output output_small = {.value = TOTAL_NUMBER_OF_COINS - size + 1 + i,
-                                                           .public_key = ((char *)(new_public_key_output_small->data))};
-
-        transaction_create_shortcut_input *inputs = malloc(2 * sizeof(transaction_create_shortcut_input));
-        inputs[0] = input_small;
-        inputs[1] = input_large;
-
-        transaction_create_shortcut create_data_small = {.num_of_inputs = 2, .num_of_outputs = 1, .outputs = &output_small, .inputs = inputs};
-
-        transaction *t_small = (transaction *)malloc(sizeof(transaction));
-
-        if (!create_new_transaction_shortcut(&create_data_small, t_small)) {
-            general_log(LOG_SCOPE, LOG_ERROR, "Failed to create a transaction. + %d", i);
-        }
-        verify_transaction(t_small);
-        if (!finalize_transaction(t_small)) {
-            general_log(LOG_SCOPE, LOG_ERROR, "Failed to finalize a transaction %d.", i);
-        } else {
-            general_log(LOG_SCOPE, LOG_INFO, "the %d round success", i);
+        if (!create_new_transaction_shortcut(&create_data, t)) {
+            general_log(LOG_SCOPE, LOG_ERROR, "Failed to create a transaction.");
         }
 
-        previous_transaction_id = get_transaction_txid(t_small);
-        previous_private_key = new_private_key_output_small;
-    }
+        previous_transaction = t;
+        previous_output_private_key = (char *)new_private_key;
 
+        block_header_shortcut block_header = {
+            .prev_block_header_hash = NULL, .version = i, .nonce = 0, .nBits = 0, .merkle_root_hash = NULL, .time = get_current_unix_time()};
+
+        memcpy(block_header.prev_block_header_hash, previous_block_header_hash, 65);
+
+        transaction **txns = malloc(sizeof(txns));
+        txns[0] = t;
+
+        transactions_shortcut txns_shortcut = {.txns = txns, .txn_count = 1};
+
+        block_create_shortcut block_data = {.header = &block_header, .transaction_list = &txns_shortcut};
+
+        block *block1 = (block *)malloc(sizeof(block));
+        if (!create_new_block_shortcut(&block_data, block1)) {
+            general_log(LOG_SCOPE, LOG_ERROR, "Failed to create a block.");
+        }
+
+        verify_block(block1);
+
+        if (i == 3) block_under_test = block1;
+
+        if (!finalize_transaction(t)) {
+            general_log(LOG_SCOPE, LOG_ERROR, "Failed to finalize a transaction.");
+        }
+
+        if (!finalize_block(block1)) {
+            general_log(LOG_SCOPE, LOG_ERROR, "Failed to finalize a block.");
+        }
+
+        previous_block_header_hash = hash_block_header(block1->header);
+    }
 
     int sock = 0, valread, client_fd;
     struct sockaddr_in serv_addr;
@@ -157,42 +111,39 @@ int main(int argc, char const *argv[]) {
         return -1;
     }
 
-    print_hex(t_sub_genesis->tx_ins[0].signature_script,64);
+    //Print block information
+    printf("Block txns count: %d\n",block_under_test->txn_count);
+    printf("Block header version: %d\n",block_under_test->header->version);
+    printf("Block header hash: ");
+    print_hex(block_under_test->header->prev_block_header_hash,64);
+    printf("Block txns[0] in[0] signature script: ");
+    print_hex(block_under_test->txns[0]->tx_ins[0].signature_script,64);
+
 
     //    transaction previous
-    socket_transaction *socket_tx = cast_to_socket_transaction(t_sub_genesis);
+    socket_block *socket_blk = cast_to_socket_block(block_under_test);
     char command[32];
     memset(command, '\0', 32);
-    memcpy(command, "create transaction", strlen("create transaction"));
+    memcpy(command, "create block", strlen("create block"));
 
-    int send_size = get_socket_transaction_length(socket_tx)+ 32;
-    char* send_socket_tx = combine_data_with_command(command,
-                                                     32,
-                                                     (const char*)socket_tx,
-                                                     get_socket_transaction_length(socket_tx));
+    int send_size = get_socket_block_length(block_under_test) + 32;
+    char *send_socket_tx = combine_data_with_command(command, 32, (const char *)socket_blk, get_socket_block_length(block_under_test));
 
-
-//    send(sock, (const char *)socket_tx, get_socket_transaction_length(socket_tx), 0);
+    //    send(sock, (const char *)socket_tx, get_socket_transaction_length(socket_tx), 0);
     send(sock, send_socket_tx, send_size, 0);
     printf("---- Client: transaction sent ----\n");
     valread = read(sock, buffer, 1024);
 
     // closing the connected socket
-    free(socket_tx);
+    free(socket_blk);
     close(client_fd);
     return 0;
 }
 
-char* combine_data_with_command(
-    char *command,
-    unsigned int command_length,
-    const char* data,
-    unsigned int data_length
-    ){
+char *combine_data_with_command(char *command, unsigned int command_length, const char *data, unsigned int data_length) {
     size_t n = command_length + data_length;
-    char *s = malloc( n );
+    char *s = malloc(n);
     memcpy(s, command, command_length);
     memcpy(s + command_length, data, data_length);
     return s;
 }
-
